3bd7b9f9ad6795948336eae38257be8d4eb59048
Christopher Pietsch <cpietsch@gmail.com>
2025-03-03 18:03:23 +0000

    update speakers data with new entries and placeholder information

--- src/data/speakers.js
+++ src/data/speakers.js
@@ -1,79 +1,56 @@
 export default [
 	{
-		name: 'Nadia Piet',
-		subtitle: 'AIxDESIGN',
-		description: `Nadia Piet is an independent designer, researcher, organizer, and educator with a focus on AI/ML, data, tech, [digital] culture and creativity. She’s the founder of AIxDESIGN [a community of practitioners & living lab for beyond-corporate AI], holds an MA in Data-Driven Design, and explores playful & purposeful tech through freelance projects and self-initiated experiments. Over the past 10 years, she’s worked as Head of Creative Technology at DEPT [a global digital agency], design researcher for emerging technologies at Bit, and across roles and continents with organizations such as Hyper Island, Pi Campus, Forbes, UN, AWWWARDS, DECODED, MOBGEN | Accenture Interactive, Mozilla, and ICO.`,
-		image: 'nadia-piet'
+		name: 'Niko Klein',
+		subtitle: 'XXX',
+		description: `XXX`,
+		image: 'niko-klein'
 	},
 	{
-		name: 'Matteo Loglio & Simone Rebaudengo',
-		subtitle: 'oio',
-		description: `oio is a creative company on a quest to turn emerging technologies into an approachable, everyday and sustainable reality. It’s a small team of humans and machines pushing for an impact that outweighs their footprint, shaping future products and narratives for what could be our future. They just launched their first product, designed in collaboration with an AI in a new process they coined as “Artisanal Intelligence”. Constantly pushing the boundaries of post-human collaborations, oio works with big companies, small startups and cultural institutions to shape products and tools for a less-boring future. Some of their collaborations include IKEA, Google, SPACE10, FABRICA, the Museum of the Future, Arduino and Samsung among others. `,
-		image: 'o-io'
+		name: 'Tobias Revell',
+		subtitle: 'XXX',
+		description: `XXX`,
+		image: 'tobias-revell'
 	},
 	{
-		name: 'Prof. Dr. Wolfgang Ertel',
-		subtitle: 'RWU Ravensburg-Weingarten',
-		description: `Prof. Dr. Wolfgang Ertel established the Institute for Artificial Intelligence at the Ravensburg-Weingarten University of Applied Sciences. He studied physics and mathematics in Konstanz and has been a researcher in artificial intelligence since 1987. His areas of expertise are machine learning and adaptive robots. After many research projects about service robots, adaptive machines and diagnostic systems he now works on AI and the future of humanity. He teaches artificial intelligence, machine learning, mathematics and sustainability. He wrote a textbook on artificial intelligence with translations into English, Chinese, Russian, Greek and Turkish. At Scientists for Future he is committed to climate and environmental protection.`,
-		image: 'wolfgang-ertel'
+		name: 'Maria Luce Lupetti',
+		subtitle: 'XXX',
+		description: `XXX`,
+		image: 'maria-luce-lupetti'
 	},
 	{
-		name: 'Ploipailin Flynn',
-		subtitle: 'antiracistby.design',
-		description: `Ploipailin Flynn is a business and product strategist interested in the design and build of radically equitable and antiracist futures. She got tired of well-designed tech products and services that turned "accidentally" racist, so she launched ANTI (<a href="http://antiracistby.design" target="_blank" rel="noopener noreferrer" class="underline">antiracistby.design</a> ) as a place of reference for business and product leads to build things that are antiracist by design. Her areas of interest include: The relationship between revenue models and discriminatory design. Antiracist and decolonial futures. Translating abstract principles into tangible products and services. Researching the intersectional impacts of Big Tech, AI, capitalism. `,
-		image: 'ploipailin-flynn'
+		name: 'Niklas Muhs',
+		subtitle: 'XXX',
+		description: `XXX`,
+		image: 'niklas-muhs'
 	},
 	{
-		name: 'Roman Stefan Grasy',
-		subtitle: 'Intuity Media Lab, Stuttgart',
-		description: `Roman Stefan Grasy is unit lead for industrial applications @ Intuity Media Lab. He has been developing digital products and services at Intuity since 2012. As expert for digital manufacturing solutions and medical technologies he coordinates multi-disciplinary teams consisting of designers, data science experts and developers.`,
-		image: 'roman-grasy'
-	},
-	{
-		name: 'Catherine Breslin',
-		subtitle: 'Conversational AI',
-		description: `Dr Catherine Breslin is a machine learning scientist and consultant based in Cambridge UK, and founder of Kingfisher Labs consulting. Since completing her PhD at the University of Cambridge in 2008, she has commercial and academic experience of automatic speech recognition, natural language understanding and human-computer dialogue systems, having previously worked at at Cambridge University, Toshiba Research, Amazon Alexa, and Cobalt Speech. She's excited by the application of research to real-world problems involving speech and language at scale.`,
-		image: 'catherine-breslin'
-	},
-
-	{
-		name: 'Simon Maris',
-		subtitle: 'AI x Design x Sustainability',
-		description: `Simon Maris is currently Professor (by proxy ~ in Vertretung) of AI - Design and Sustainability at the Department of Art and Design at the University of Applied Sciences Trier. He explores, questions, makes. Trained as an architect, he has long been fascinated by technology and it's intersection with society and art, with a particular focus on computational design, digital fabrication, robotics and more recently AI systems. He co-hosts the towards co-creation podcast.`,
-		image: 'simon-maris'
-	},
-
-	{
-		name: 'Emily Saltz',
-		subtitle: 'UX Google Jigsaw',
-		description: `Emily Saltz is a UX Researcher at Google Jigsaw, working on tools for platforms and moderators to address online harms. Before that, she was a UX Researcher at the New York Times R&D Lab, conducting research on topics ranging from media credibility (the News Provenance Project), to NLP Q&A tools. She was a 2020 Fellow at the Partnership on AI studying manipulated and synthetic media, where she co-authored "The Responsible AI Art Field Guide." She holds a Master’s in Computer-Interaction from Carnegie Mellon, and a BA in Linguistics from UC Santa Cruz, and is Part-Time Faculty in Design & Technology at Parsons.`,
-		image: 'emily-saltz'
-	},
-	{
-		name: 'Tom White',
-		subtitle: 'Artist & Researcher',
-		description: `Tom White (dribnet) is a New Zealand based artist investigating machine perception. His current work focuses on creating physical artworks that highlight how machines “see” and thus how they think, suggesting that these systems are capable of abstraction and conceptual thinking. He has exhibited computer based artwork internationally over the past 25 years with themes of artificial intelligence, interactivity, and computational creativity. He is currently a lecturer and researcher at University of Wellington School of Design where he teaches students the creative potential of computer programming and artificial intelligence.`,
-		image: 'tom-white'
+		name: 'Elisa Giaccardi',
+		subtitle: 'XXX',
+		description: `XXX`,
+		image: 'elisa-giaccardi'
 	},
 	{
 		name: 'Andreas Refsgaard',
-		subtitle: 'Artist',
-		description: `Andreas Refsgaard is an artist and creative coder based in Copenhagen. Working in the field between art and interaction design he uses algorithms, coding and machine learning to explore the creative potentials of emerging digital technologies.
-		His works have been published in New Scientist, Vice, Gizmodo, PSFK and Designboom, awarded by Interaction Awards and Core77 and exhibited in museums and at festivals in Europe, North America, Asia and Australia.`,
+		subtitle: 'XXX',
+		description: `XXX`,
 		image: 'andreas-refsgaard'
 	},
 	{
-		name: 'Norbert Riedelsheimer',
-		subtitle: 'IBM Client Engineering',
-		description: `Norbert Riedelsheimer is working as global Design Program Director at IBM Client Engineering, a business unit that leverages cutting edge AI and Cloud Technologies in interdisciplinary squad teams to innovative with IBMs most important clients.
-		As regulations like the EU AI-Act are on the horizon, clients from industries like banking, insurance, industrial, distribution and the public sector are interested in AI-solutions that are trustworthy, explainable and fair.
-		Norbert thinks that we as designers need to even push beyond that and have the power to maximize the positive impact of AI Technologies by applying them in context of the worlds bigger challenges.`,
-		image: 'norbert-riedelsheimer'
+		name: 'Julian Oliver',
+		subtitle: 'XXX',
+		description: `XXX`,
+		image: 'julian-oliver'
 	},
 	{
-		name: 'Team Prompt Battle',
-		subtitle: 'HTW Dresden',
-		description: `Prompt Battle Delegation: Bernadette Geiger & Ella Zickerick <br/><br/> Prompt Battle is a live event where people compete against each other using text-to-image software. Show off your prompt skills and maybe the audience will choose you as the winner who elicited the most surprising, disturbing or beautiful images from the latent spaces of DALL·E 2, Stable Diffusion, Midjourney, Craiyon, etc. <br/><br/>The Prompt Battle format was developed by Florian A. Schmidt & Sebastian Schmieg with the design students at HTW Dresden: Bernadette Geiger, Ella Zickerick, Emily Krause, Levi Stein, Lina Schwarzenberg, Robert Hellwig`,
-		image: 'prompt-battle'
+		name: 'Iohanna Nicenboim',
+		subtitle: 'XXX',
+		description: `XXX`,
+		image: 'iohanna-nicenboim'
+	},
+	{
+		name: 'Eryk Salvaggio',
+		subtitle: 'XXX',
+		description: `XXX`,
+		image: 'eryk-salvaggio'
 	}
 ];
